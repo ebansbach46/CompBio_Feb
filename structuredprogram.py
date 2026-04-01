e65fa1ccb998c86f6758dd7eb46fb8ddd6afe516
ebansbach46 <ebansbach46@gmail.com>
2026-04-01 15:01:21 -0400

    fixed hw 6

---
+++ structuredprogram.py
@@ -0,0 +1,11 @@
+# structured programming
+# E Bansbach
+# 3.31.26
+
+# reinstall jupyter into the environment go to terminal and install (conda install jupyter)  
+# do this to render qmd python into html
+# ctl shift k to render the html file
+
+# data folder with data sets inside-know exactly where they are 
+# functions are called by the driver script
+
